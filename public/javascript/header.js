bd6a04340c2131bb1b27173d612d0cdf7aaa256e
damatoaj <mr.arthurdamato@gmail.com>
2021-09-22 13:43:34 -0700

    added an animation

---
+++ public/javascript/header.js
@@ -0,0 +1,35 @@
+let activeLinks = document.querySelectorAll('.navLi')
+let bannerId = document.getElementById('bannerId');
+let splitText = bannerId.textContent.split('');
+let timer;
+let char = 0;
+
+if(bannerId.textContent) {
+    console.log('someone signed in')
+    bannerIdAnimation(bannerId);
+    console.log('ran equation')
+}
+function bannerIdAnimation(elem) {
+    timer = setInterval(onTick, 100)
+    bannerId.textContent = '';
+    for (let char = 0; char < splitText.length; char++) {        
+        bannerId.innerHTML += "<span>" + splitText[char] + "</span>";
+    }
+}
+
+function onTick(){
+    const span = bannerId.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+    console.log(char)
+    if(char == splitText.length) {
+        complete();
+        return;
+    }
+}
+
+function complete() {
+    console.log('run complete')
+    clearInterval(timer);
+    timer = null;
+};
