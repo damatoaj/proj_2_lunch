e4f5aa2381d7edad0120cdb004f3b638e434b183
damatoaj <mr.arthurdamato@gmail.com>
2021-09-27 11:01:40 -0700

    css changes

--- public/javascript/alerts.js
+++ public/javascript/alerts.js
@@ -3,13 +3,12 @@ let alertError = document.getElementById('alert-container error');
 let alertMessage = document.getElementById('alert');
 
 if (alertMessage) {
-    console.log('alerts be gone')
+    
     setTimeout(endAlerts, 5000)
-    console.log('alerts are gone')
 };
 
 function endAlerts() {
-    if(alertSuccess && alertSuccess.style?.display) {alertSuccess.style.display = 'none'};
-    if(alertError && alertError.style?.display) {alertError.style.display = 'none'};
-    if(alertMessage && alertMessage.style?.display) {alertMessage.style.display = 'none'};
+    if(alertSuccess && alertSuccess.style) {alertSuccess.style.display = 'none'};
+    if(alertError && alertError.style) {alertError.style.display = 'none'};
+    if(alertMessage && alertMessage.style) {alertMessage.style.display = 'none'};
 };
